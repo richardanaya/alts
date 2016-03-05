9e1df68169d2f62719153ee32e54208eb170c43b
Admin <admin@Guests-MacBook-Pro.local>
2016-03-04 21:20:19 -0600

    implementing alts

--- alts.js
+++ alts.js
@@ -6,7 +6,28 @@
   "use strict";
 
   function alts(){
+    var isResolved = false;
+    var rejectCount = 0;
 
+    var promises = Array.prototype.slice.call(arguments);
+    function watchPromise(promise,resolve, reject){
+      promise.then(function(result){
+        if(!isResolved){
+          isResolved=true;
+          resolve({source:promise,value:result});
+        }
+      }).catch(function(){
+        rejectCount++;
+        if(rejectCount==promises.length){
+          reject(promises);
+        }
+      })
+    }
+
+    var p = new Promise(function(resolve,reject){
+      promises.forEach(function(p){watchPromise(p,resolve,reject);})
+    })
+    return p;
   }
 
   window.alts = module.exports = alts;
