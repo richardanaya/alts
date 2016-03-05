209675cfb12d755efd349dc5228d536f73ff31b1
Admin <admin@Guests-MacBook-Pro.local>
2016-03-04 20:54:19 -0600

    first commit

---
+++ alts.js
@@ -0,0 +1,16 @@
+// alts.js
+// repo    : https://github.com/richardanaya/alts
+// license : MIT
+
+(function (window, module) {
+  "use strict";
+
+  function alts(){
+
+  }
+
+  window.alts = module.exports = alts;
+})(
+  typeof window !== "undefined" ? window : {},
+  typeof module !== "undefined" ? module : {}
+);
